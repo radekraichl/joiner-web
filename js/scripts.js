3d82c08cacd857897d84a0d2e9b30d9474d0149a
Radek Raichl <radekraichl@gmail.com>
2022-11-27 19:59:21 +0100

    move javascript from index.html to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
+// lightgallery init
+lightGallery(document.getElementById('light-gallery'), {
+    plugins: [lgThumbnail, lgFullscreen, lgZoom],
+    licenseKey: 'your_license_key',
+    getCaptionFromTitleOrAlt: false,
+    startAnimationDuration: 300,
+    hideBarsDelay: 5000,
+    actualSize: true,
+    fullScreen: true,
+    counter: false,
+    mode: 'lg-tube',
+    speed: 500,
+    zoom: true,
+    scale: 1
+});
+
+// set fade for hero carousel images
+setHeroCarouselFadeTime(0.4);                                    // first fade
+setTimeout(function () { setHeroCarouselFadeTime(2.4) }, 1);     // all the others (after the set time)
+
 // this will set the fade time for the hero carousel images
-function setHeroCarouselFadeTime(time) {
+function setHeroCarouselFadeTime(time)
+{
     createCSSSelector
         ('.carousel-item', `transition: transform ${time}s ease-in-out`);
     createCSSSelector
@@ -9,26 +30,33 @@ function setHeroCarouselFadeTime(time) {
 }
 
 // this will create a new css rule
-function createCSSSelector(selector, style) {
+function createCSSSelector(selector, style)
+{
     if (!document.styleSheets) return;
     if (document.getElementsByTagName('head').length == 0) return;
 
     var styleSheet, mediaType;
 
-    if (document.styleSheets.length > 0) {
-        for (var i = 0, l = document.styleSheets.length; i < l; i++) {
+    if (document.styleSheets.length > 0)
+    {
+        for (var i = 0, l = document.styleSheets.length; i < l; i++)
+        {
             if (document.styleSheets[i].disabled)
                 continue;
             var media = document.styleSheets[i].media;
             mediaType = typeof media;
 
-            if (mediaType === 'string') {
-                if (media === '' || (media.indexOf('screen') !== -1)) {
+            if (mediaType === 'string')
+            {
+                if (media === '' || (media.indexOf('screen') !== -1))
+                {
                     styleSheet = document.styleSheets[i];
                 }
             }
-            else if (mediaType == 'object') {
-                if (media.mediaText === '' || (media.mediaText.indexOf('screen') !== -1)) {
+            else if (mediaType == 'object')
+            {
+                if (media.mediaText === '' || (media.mediaText.indexOf('screen') !== -1))
+                {
                     styleSheet = document.styleSheets[i];
                 }
             }
@@ -38,12 +66,15 @@ function createCSSSelector(selector, style) {
         }
     }
 
-    if (typeof styleSheet === 'undefined') {
+    if (typeof styleSheet === 'undefined')
+    {
         var styleSheetElement = document.createElement('style');
         document.getElementsByTagName('head')[0].appendChild(styleSheetElement);
 
-        for (i = 0; i < document.styleSheets.length; i++) {
-            if (document.styleSheets[i].disabled) {
+        for (i = 0; i < document.styleSheets.length; i++)
+        {
+            if (document.styleSheets[i].disabled)
+            {
                 continue;
             }
             styleSheet = document.styleSheets[i];
@@ -52,19 +83,25 @@ function createCSSSelector(selector, style) {
         mediaType = typeof styleSheet.media;
     }
 
-    if (mediaType === 'string') {
-        for (var i = 0, l = styleSheet.cssRules.length; i < l; i++) {
-            if (styleSheet.cssRules[i].selectorText && styleSheet.cssRules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
+    if (mediaType === 'string')
+    {
+        for (var i = 0, l = styleSheet.cssRules.length; i < l; i++)
+        {
+            if (styleSheet.cssRules[i].selectorText && styleSheet.cssRules[i].selectorText.toLowerCase() == selector.toLowerCase())
+            {
                 styleSheet.cssRules[i].style.cssText = style;
                 return;
             }
         }
         styleSheet.insertRule(selector, style);
     }
-    else if (mediaType === 'object') {
+    else if (mediaType === 'object')
+    {
         var styleSheetLength = styleSheet.cssRules ? styleSheet.cssRules.length : 0;
-        for (var i = 0; i < styleSheetLength; i++) {
-            if (styleSheet.cssRules[i].selectorText && styleSheet.cssRules[i].selectorText.toLowerCase() == selector.toLowerCase()) {
+        for (var i = 0; i < styleSheetLength; i++)
+        {
+            if (styleSheet.cssRules[i].selectorText && styleSheet.cssRules[i].selectorText.toLowerCase() == selector.toLowerCase())
+            {
                 styleSheet.cssRules[i].style.cssText = style;
                 return;
             }
@@ -72,3 +109,7 @@ function createCSSSelector(selector, style) {
         styleSheet.insertRule(selector + '{' + style + '}', styleSheetLength);
     }
 }
+
+// fix flickering intro image
+const heroFirstImage = document.getElementById('hero-first-img');
+heroFirstImage.className += ' active';
