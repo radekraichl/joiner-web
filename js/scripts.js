8854e87a5f90653ef363f631d37c9ded27245864
Radek Raichl <radekraichl@gmail.com>
2022-12-02 18:09:34 +0100

    upadate jarallax settigs

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,36 @@
-// Lightgallery init
-lightGallery(document.getElementById('light-gallery'), {
-    plugins: [lgThumbnail, lgFullscreen, lgZoom],
-    licenseKey: 'your_license_key',
-    getCaptionFromTitleOrAlt: false,
-    startAnimationDuration: 300,
-    hideBarsDelay: 5000,
-    actualSize: true,
-    fullScreen: true,
-    counter: false,
-    mode: 'lg-tube',
-    speed: 500,
-    zoom: true,
-    scale: 1
-});
-
-// Set fade time
-$(document).ready(function ()
+$(function ()
 {
-    $("<style> .carousel-item { transition: opacity 3s ease-in-out } </style>").appendTo("head")
-    $("<style> .carousel-fade .active.carousel-item-end { transition: opacity 0s 3s } </style>").appendTo("head")
-    $("<style> .carousel-fade .active.carousel-item-start { transition: opacity 0s 3s } </style>").appendTo("head")
+    // Jarallax init
+    $('.jarallax').jarallax({
+        speed: 0.01,
+        imgSize: 'cover'
+    });
+
+    var j = $('.jarallax').first();
+
+    console.log(j[0].jarallax.pureOptions);
+
+    // Set carousel fade time
+    $('<style> .carousel-item { transition: opacity 3s ease-in-out } </style>').appendTo('head');
+    $('<style> .carousel-fade .active.carousel-item-end { transition: opacity 0s 3s } </style>').appendTo('head');
+    $('<style> .carousel-fade .active.carousel-item-start { transition: opacity 0s 3s } </style>').appendTo('head');
+
+    // Lightgallery init
+    lightGallery($('#light-gallery').get(0), {
+        plugins: [lgThumbnail, lgFullscreen, lgZoom],
+        licenseKey: 'your_license_key',
+        getCaptionFromTitleOrAlt: false,
+        startAnimationDuration: 300,
+        hideBarsDelay: 5000,
+        actualSize: true,
+        fullScreen: true,
+        counter: false,
+        mode: 'lg-tube',
+        speed: 500,
+        zoom: true,
+        scale: 1,
+    });
 });
 
 // Fix flickering intro image
-const heroFirstImage = document.getElementById('hero-first-img');
-heroFirstImage.className += ' active';
+$('#hero-first-img').addClass('active');
